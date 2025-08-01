e8d73ee543d69b1c6aafe1347882f04b7bd3fdc9
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-01 22:17:55 +0530

    time to code the backend

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93075a2b9b9c2060489c4fa831e6077066ab41da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d35252e64bff4145681f83c965de26425621c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
